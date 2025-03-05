7f396654d04e51bddf6e6b59207922c0c00c96f2
KraksOficial <frankapinor@gmail.com>
2025-03-05 17:52:43 -0400

    Update latest-version.php

--- latest-version.php
+++ latest-version.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json'); // Indicar que la respuesta es JSON
 // Datos de la última versión
 $latestVersion = array(
     "version" => "1.0.0", // Última versión disponible
-    "apkUrl" => "https://drive.usercontent.google.com/download?id=1iV_TE7CqF6FlzBH9YUrAgQ6vBHs5SLSB&export=download&authuser=0&confirm=t&uuid=40c420df-c2a5-42c1-865b-1a2884f429c1&at=AEz70l5dyL6cotjj267oFE5E8Y6d:1741210787536" // URL del APK
+    "apkUrl" => "https://bit.ly/dolarvenezuelaapk" // URL del APK
 );
 
 // Devolver los datos en formato JSON
